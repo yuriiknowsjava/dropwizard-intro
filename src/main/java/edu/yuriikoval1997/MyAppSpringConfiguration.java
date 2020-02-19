baa35bffb9ea4d4e8ad1f1ab15c28403db643590
yuriiknowsjava <yuriiknowsjava@gmail.com>
2020-02-19 18:32:21 +0200

    Removed useless basePackages

--- src/main/java/edu/yuriikoval1997/MyAppSpringConfiguration.java
+++ src/main/java/edu/yuriikoval1997/MyAppSpringConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Configuration
-@ComponentScan(basePackages = {"edu.yuriikoval1997", "edu.yuriikoval1997.configs"})
+@ComponentScan
 public class MyAppSpringConfiguration {
 
     @Bean
